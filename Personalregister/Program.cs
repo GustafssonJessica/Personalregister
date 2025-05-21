fe0ecbe787a06dbcfe8c29a2e4a7e47a42bea566
GustafssonJessica <jessica.gustafsson@live.se>
2025-05-21 21:04:51 +0200

    Add project files.

---
+++ Personalregister/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personalregister
+{
+    internal class Program
+    {
+
+
+        /*
+        //Klasser som bör implementeras:
+            Personal är väl den enda som är nödvändig utifrån deras krav.
+            Sedan kan det kanske om de blir större vara trevligt att ha en Restaurang-klass, om de skaffar flera restauranger.
+            Leverantörer
+            Utrustning
+            Maträtter
+            Bokningar
+
+        Det skulle kanske vara bra med olika typer av underklasser till personal om de blir större i framtiden. 
+        Exempelvis administrativ personal, kökspersonal, serveringspersonal. Tyvärr minns jag inte hur man skapar underklasser dock.
+
+
+        Attribut som bör ingå i klasserna:
+
+        Förnamn, Efternamn, födelsedag, bostadsadress, kontaktuppgifter till närmast anhörig, mejladress, telefonnummer, hur länge personenen varit anställd, datum som personen ska vara ledig,
+        "behörigheter" och övrig info (ex om får hantera känsliga livsmedel, ha ansvar för kassa osv)
+        Metoder: Get och Set. Kanske skicka mejl eller ringa. 
+        Sen måste man såklart kunna ta bort personal med, men tror inte det är en metod i själva klassen. 
+
+        */
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
